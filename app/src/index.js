7f1fbcdbcd3f69d18cfb256ce6e17a45d6d562f0
Samarth Patel <samarth.patel.22@hotmail.com>
2019-05-10 09:40:32 -0400

    Created basic functionality

---
+++ app/src/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import marked from 'marked';
+
+class Output extends React.Component {
+  constructor(props) {
+    super(props);
+    this.rawMarkup = this.rawMarkup.bind(this);
+  }
+
+  rawMarkup() {
+    let rawMarkup = marked(this.props.text, {sanitize: true});  
+    return {__html: rawMarkup};
+  }
+
+  render() {
+    return (
+      <div className='output size' dangerouslySetInnerHTML={this.rawMarkup()}>
+    </div>
+    )
+  }
+}
+
+class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({text: e.target.value});
+  }
+
+  render() {
+    return (
+      <div>
+        <textarea className='input size' value={this.state.value} onChange={this.handleChange}/>
+        <Output text={this.state.text} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Editor />,
+  document.getElementById('root')
+)
